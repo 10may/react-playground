de34e56ff975d5eebb3501bbfc52036d6b5f9818
Tanmay <tanmayb@caphillbrands.com>
2023-07-13 22:10:25 +0530

    remove stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { ThemeProvider } from 'styled-components';
 
-import { MultipleSelect } from './component/MultipleSelect';
 import { theme } from './theme';
 
 function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<div className='flex h-screen items-center justify-center'>
-				<MultipleSelect />
+                React Playground
 			</div>
 		</ThemeProvider>
 	);
--- src/types/index.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
-export type Prettify<T> = unknown & {
-	[K in keyof T]: T[K];
-};
