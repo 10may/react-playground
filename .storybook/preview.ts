d81c421b9f7882faf8b64590e394f6775dee08fe
Tanmay Bindal <10maybinda@gmail.com>
2024-03-20 19:23:55 +0530

    add prettier

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,26 +1,28 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
 import type { Preview } from '@storybook/react';
+
 import '../src/index.css';
 
-import { withThemeByClassName } from "@storybook/addon-themes";
-
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 
-  decorators: [withThemeByClassName({
-      themes: {
-          // nameOfTheme: 'classNameForTheme',
-          light: '',
-          dark: 'dark',
-      },
-      defaultTheme: 'light',
-  })]
+    decorators: [
+        withThemeByClassName({
+            themes: {
+                // nameOfTheme: 'classNameForTheme',
+                light: '',
+                dark: 'dark',
+            },
+            defaultTheme: 'light',
+        }),
+    ],
 };
 
 export default preview;
