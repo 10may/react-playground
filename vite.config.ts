11d26fec4757ee004642a7f9bef23d5d0baa90ca
Tanmay Bindal <10maybinda@gmail.com>
2024-03-20 19:53:30 +0530

    remove vite-aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import { ViteAliases } from 'vite-aliases';
+
+// import { ViteAliases } from 'vite-aliases';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), ViteAliases()],
+    plugins: [
+        react(),
+        // ViteAliases()
+    ],
 });
