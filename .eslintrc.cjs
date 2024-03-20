80af442b828c9a8997fec7e1917b668471b2992b
Tanmay Bindal <10maybinda@gmail.com>
2024-03-20 19:27:37 +0530

    update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,144 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
+
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
+
+        // 'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+
         'plugin:storybook/recommended',
+
+        'plugin:import/recommended',
+        'plugin:import/typescript',
+
+        'plugin:tailwindcss/recommended',
+
+        'prettier',
     ],
-    ignorePatterns: ['dist', '.eslintrc.cjs'],
+
     parser: '@typescript-eslint/parser',
-    plugins: ['react-refresh'],
+
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+
+    plugins: [
+        '@typescript-eslint',
+        'import',
+        'react',
+        'react-refresh',
+        'unused-imports',
+    ],
+
     rules: {
-        'react-refresh/only-export-components': [
+        // default
+        indent: 'off',
+        quotes: 'off',
+        semi: 'off',
+        'linebreak-style': 'off',
+
+        // React
+        'react/prop-types': 'off',
+        'react-refresh/only-export-components': 'warn',
+
+        // tailwind
+        'tailwindcss/no-custom-classname': [
             'warn',
-            { allowConstantExport: true },
+            {
+                callees: ['classnames', 'clsx', 'ctl', 'tv'],
+            },
+        ],
+
+        // import
+        'import/default': 'warn',
+        'import/no-cycle': 'warn',
+        'import/no-duplicates': 'warn',
+        'import/no-named-as-default': 'off',
+        'import/no-self-import': 'warn',
+        'import/no-unassigned-import': 'off',
+        'import/no-unresolved': 'error',
+        'import/no-useless-path-segments': 'warn',
+
+        // typescript
+        '@typescript-eslint/consistent-type-exports': 'warn',
+        '@typescript-eslint/consistent-type-imports': [
+            'warn',
+            {
+                fixStyle: 'inline-type-imports',
+            },
+        ],
+
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-misused-promises': [
+            'error',
+            {
+                checksVoidReturn: {
+                    arguments: false,
+                    attributes: false,
+                },
+            },
+        ],
+        '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
+        '@typescript-eslint/sort-type-constituents': 'warn',
+
+        /* unused imports */
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            {
+                vars: 'all',
+                varsIgnorePattern: '^_',
+                args: 'after-used',
+                argsIgnorePattern: '^_',
+            },
         ],
     },
+
+    settings: {
+        react: {
+            version: 'detect',
+        },
+
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true,
+                // project: ['./tsconfig.json', './tsconfig.node.json'],
+            },
+        },
+    },
 };
+
+/*
+ni -D @typescript-eslint/eslint-plugin \
+@typescript-eslint/parser \
+eslint \
+eslint eslint-config-prettier \
+eslint-import-resolver-typescript \
+eslint-plugin-import@npm:eslint-plugin-i \
+eslint-plugin-react \
+eslint-plugin-react-hooks \
+eslint-plugin-unused-imports \
+eslint-plugin-tailwindcss
+*/
